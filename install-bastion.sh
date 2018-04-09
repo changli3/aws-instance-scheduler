6d90187cd4293bd416ad34ad4fb9428894f9c482
Chang Li <chang.li3@treasury.gov>
2018-04-09 18:32:18 -0400

    updated install-bastion.sh

--- install-bastion.sh
+++ install-bastion.sh
@@ -6,7 +6,7 @@ wget https://github.com/changli3/awsd-instance-scheduler/raw/master/scheduler-cl
 unzip scheduler-cli.zip
 python ./setup.py install
 
-wget https://raw.githubusercontent.com/changli3/awsd-instance-scheduler/master/get-tag-csv.sh
-wget https://raw.githubusercontent.com/changli3/awsd-instance-scheduler/master/tag-csv.py
+curl -O  https://raw.githubusercontent.com/changli3/awsd-instance-scheduler/master/get-tag-csv.sh
+curl -O  https://raw.githubusercontent.com/changli3/awsd-instance-scheduler/master/tag-csv.py
 chmod +x ./get-tag-csv.sh
 
